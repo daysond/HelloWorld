e489381433bb67dfb670261f8d9f2ec9b943cc7c
dayson dong <daysond@outlook.com>
2019-01-24 02:28:31 -0500

    Initial Commit

---
+++ HelloWorld/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HelloWorld
+//
+//  Created by Dayson Dong on 2019-01-24.
+//  Copyright © 2019 Dayson Dong. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
